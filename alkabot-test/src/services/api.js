6e0ff394eb386b4245afb3ef2c03baaaed797bda
Eduardo Caper <eduardo.caper@uy3.com.br>
2023-03-24 03:18:54 -0300

    lint and prettier working

---
+++ alkabot-test/src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const getPosts = async () => {
+  const response = await axios.get(`${BASE_URL}/posts`);
+  return response.data;
+};
+
+export const getPostComments = async (postId) => {
+  const response = await axios.get(`${BASE_URL}/posts/${postId}/comments`);
+  return response.data;
+};
+
+export const getUsers = async () => {
+  const response = await axios.get(`${BASE_URL}/users`);
+  return response.data;
+};
+
+export const getUserDetails = async (userId) => {
+  const response = await axios.get(`${BASE_URL}/users/${userId}`);
+  return response.data;
+};
